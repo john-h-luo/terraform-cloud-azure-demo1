c05d5bb95783f84bc8d5e3ab5d3a4f4a03012196
john-h-luo <jiehua.luo@zilliz.com>
2023-04-17 18:12:36 +0800

    Changed resource to be created

---
+++ main.tf
@@ -0,0 +1,41 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.15.00"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resourcegroup_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each = var.subnets
+  resource_group_name  = var.resourcegroup_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  name                 = each.value["name"]
+  address_prefixes     = each.value["address_prefixes"]
+}
